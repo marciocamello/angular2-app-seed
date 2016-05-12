f14d1707eec34fcb2c035b4896fbaf27b4213f94
marcio a avelino do vale <marciocamello@outlook.com>
2016-05-11 23:22:56 -0300

    update rc

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,10 +1,11 @@
-import {Component} from 'angular2/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
+        <h1>Angular 2 App</h1>
         <p>Hello World!</p>
+       
     `,
 })
 export class AppComponent {
--- gulpfile.js
+++ gulpfile.js
@@ -6,31 +6,27 @@ var assetsProd = 'src/';
 var appDev = 'dev/';
 var appProd = 'app/';
 
-/* Mixed */
-var ext_replace = require('gulp-ext-replace');
-
 /* CSS */
-var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('autoprefixer');
-var precss = require('precss');
-var cssnano = require('cssnano');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var cssClean = require('gulp-clean-css');
 
 /* JS & TS */
-var jsuglify = require('gulp-uglify');
 var typescript = require('gulp-typescript');
-
-/* Images */
-var imagemin = require('gulp-imagemin');
 
 var tsProject = typescript.createProject('tsconfig.json');
 
 gulp.task('build-css', function () {
     return gulp.src(assetsDev + 'scss/*.scss')
         .pipe(sourcemaps.init())
-        .pipe(postcss([precss, autoprefixer, cssnano]))
+        .pipe(sass({}).on('error', sass.logError))
         .pipe(sourcemaps.write())
-        .pipe(ext_replace('.css'))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe(cssClean({compatibility: 'ie8'}))
         .pipe(gulp.dest(assetsProd + 'css/'));
 });
 
@@ -43,23 +39,28 @@ gulp.task('build-ts', function () {
         .pipe(gulp.dest(appProd));
 });
 
-gulp.task('build-img', function () {
-    return gulp.src(assetsDev + 'img/**/*')
-        .pipe(imagemin({
-            progressive: true
-        }))
-        .pipe(gulp.dest(assetsProd + 'img/'));
-});
+gulp.task('bundle-ts', ['build-ts'], function() {
+    var path = require("path");
+    var Builder = require('systemjs-builder');
 
-gulp.task('build-html', function () {
-    return gulp.src(appDev + '**/*.html')
-        .pipe(gulp.dest(appProd));
+// optional constructor options
+// sets the baseURL and loads the configuration file
+    var builder = new Builder('', 'systemjs.config.js');
+
+    builder
+        .buildStatic('app/boot.js', 'app/bundle.js', { minify: true, sourceMaps: true})
+        .then(function() {
+            console.log('Build complete');
+        })
+        .catch(function(err) {
+            console.log('Build error');
+            console.log(err);
+        });
 });
 
 gulp.task('watch', function () {
     gulp.watch(appDev + '**/*.ts', ['build-ts']);
     gulp.watch(assetsDev + 'scss/**/*.scss', ['build-css']);
-    gulp.watch(assetsDev + 'img/*', ['build-img']);
 });
 
-gulp.task('default', ['watch', 'build-ts', 'build-css']);
+gulp.task('default', ['watch', 'build-ts', 'bundle-ts', 'build-css']);
